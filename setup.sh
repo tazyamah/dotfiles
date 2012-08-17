7f691c075217d6090feabb7e0770c1f976cce1c6
Tatsuya Suzuki <tazyamah@gmail.com>
2012-08-18 02:56:13 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash -v
+
+# dotfiles
+ln -vsf ~/dotfiles/.vimrc ~/
+ln -vsf ~/dotfiles/.gitconfig ~/
+ln -vsf ~/dotfiles/.gitignore ~/
+ln -vsf ~/dotfiles/.profile ~/
+
+# NeoBundle(vim)
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+# application support
+mkdir -p ~/Library/Application\ Support/KeyRemap4MacBook
+ln -vsf ~/dotfiles/application_supprt/private.xml ~/Library/Application\ Support/KeyRemap4MacBook/private.xml
+
